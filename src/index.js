7813b54a53c8dd4aa7a97d46dfbfd1c71838f2a7
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2023-03-04 12:30:43 +0530

    Done basic setup for booking service

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const express = require('express')
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const {PORT} = require('./config/serverConfig');
+
+const apiRoutes = require('./routes/index');
+
+const db = require('./models/index');
+
+const SetupAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',apiRoutes);
+    app.listen(PORT, () =>{
+        console.log(`Server start on PORT ${PORT}`);
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter:true});
+        }
+    });
+}
+
+SetupAndStartServer();
